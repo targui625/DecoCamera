4b02b2615d3a5c8cba60a4a9aa281c3203c26e1f
高木則迪 <dc667_tg@yahoo.co.jp>
2016-01-18 12:02:10 +0900

    Initial Commit

---
+++ DecoCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DecoCamera
+//
+//  Created by Norimichi Takagi on 2016/01/18.
+//  Copyright © 2016年 NorimichiTakagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
